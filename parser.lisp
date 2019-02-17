d2dc32388943109b6273d22810d6b1f87481c76b
Ryos Suzuki <ryos@sunra.sinby.com>
2019-02-17 08:50:38 +0000

    one-maarkdown-to-func line version.

--- parser.lisp
+++ parser.lisp
@@ -1,10 +1,22 @@
-(defun one-markdown-to-func (line)
-  (multiple-value-bind (match regs)
-      (cl-ppcre:scan-to-strings "^([^0-9a-zA-Z]*) +(.*)" line)
 
-    (let* ((flst
-            (map 'list #'(lambda (x) (string-trim '(#\Space #\Tab #\Newline) x))))
-           (fname (car flst)))
+(defun |mw/*| (x) `(h1 ,x))
+(defun |mw/**| (x) `(h2 ,x))
+(defun |mw/***| (x) `(h3 ,x))
+
+(defun one-markdown-to-func (strm)
+  (multiple-value-bind (match regs)
+      (cl-ppcre:scan-to-strings "^([^0-9a-zA-Z]*) *(.*)" line)
+
+    (let* ((flstv
+            (map 'vector #'(lambda (x) (string-trim '(#\Space #\Tab #\Newline) x)) regs))
+           (jgeil (print `(:jgeil ,flstv ,regs)))
+           (fname (intern (string-concat "mw/" (elt flstv 0))))
+           (an-arg (elt flstv 1))
+           (flst (list fname an-arg)))
+
       (if (fboundp fname)
-        (eval flst)
+        (progn
+          (print `(:flst ,flst))
+          (eval flst))
         (cadr flst)))))
+
